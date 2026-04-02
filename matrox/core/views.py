2313b60d0fc5683068f2b65216f13c1b11840844
ktbm <boliventtalla12@gmail.com>
2026-04-02 11:51:25 +0100

    update

--- matrox/core/views.py
+++ matrox/core/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 def home(request):
-    return HttpResponse("Bienvenue sur mon projet Django 🚀")
+    return render(request, "home.html")
