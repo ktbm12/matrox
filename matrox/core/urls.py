86ffe5a26fc29a80565cee7ee70c51385260e85b
ktbm <boliventtalla12@gmail.com>
2026-04-02 12:38:09 +0100

    update

--- matrox/core/urls.py
+++ matrox/core/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('service/', views.service, name='service'),
+    path('liste/', views.liste, name='liste'),
+    path('detail/', views.detail, name='detail'),
 ]
--- matrox/core/views.py
+++ matrox/core/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def service(request):
+    return render(request, "service.html")
+
+
+def liste(request):
+    return render(request, "liste.html")
+
+
+def detail(request):
+    return render(request, "detail.html")
