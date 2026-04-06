3d50a8491ca52464ef5b9f85709a4f94aa4813ed
ktbm <boliventtalla12@gmail.com>
2026-04-06 05:39:33 +0100

    k

--- matrox/core/templatetags/custom_tags.py
+++ matrox/core/templatetags/custom_tags.py
@@ -1,7 +1,12 @@
 from django import template
+from core.models import Settings
 
 register = template.Library()
 
 @register.filter(name='split')
 def split(value, arg):
     return value.split(arg)
+
+@register.simple_tag
+def get_global_settings():
+    return Settings.objects.first()
